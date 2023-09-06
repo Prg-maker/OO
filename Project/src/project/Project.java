a3922e01ec0abf4d6083aa9089fcf0942d1af18a
Prg-maker <df3296709@gmail.com>
2023-09-05 21:17:45 -0300

    fist commit

---
+++ Project/src/project/Project.java
@@ -0,0 +1,16 @@
+package project;
+
+ 
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class Project  {
+	public static void main(String[] args) { 	
+		String[] fruts = {"Maça" , "Uva", "Peira"};
+		
+		for (String d:fruts) {
+			System.out.println(d);
+		}
+	}
+	
+}
